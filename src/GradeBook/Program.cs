9a765106fdf536fa83a4068fa678d79f9b1a5a1c
Sagar Gill <sagargill@hotmail.co.uk>
2021-03-04 10:15:27 +0000

    hello

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = 34.1;
+            var y = 10.3;
+            var result = x + y;
+            Console.WriteLine(result);
+
+
+            if(args.Length > 0)
+            {
+            Console.WriteLine($"Hello, {args[0]}!");
+            }
+            else
+            {
+            Console.WriteLine("Hello!");
+            }
+        }
+    }
+}
